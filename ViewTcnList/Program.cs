ed431056c86a976b768de5331cb0f18edab68b64
Billy Delgado <billytagzz@gmail.com>
2018-07-08 22:34:45 -0300

    Add logic to show list to ViewTcnList Project

--- ViewTcnList/Program.cs
+++ ViewTcnList/Program.cs
@@ -1,8 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ProcessTcns;
+using ProcessTcns.ListTcnByMode;
 
 namespace ViewTcnList
 {
@@ -10,7 +9,28 @@ namespace ViewTcnList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var path = @"C:\TCNs.txt";
+            var tcnsFromTxtFile = new TcnsFromTxtFile(path);
+            var baseTcnList = new List<TcnDto>(tcnsFromTxtFile.GetList());
+
+            Console.WriteLine("***Original List ****\n");
+            foreach (var t in baseTcnList)
+                Console.WriteLine($"{t.Id} {t.Tcn}");
+
+            var tcnsByMaxMode = new TcnsByMaxMode();
+            var tcnsByMinMode = new TcnsByMinMode();
+
+            var MaxTcnList = new List<TcnDto>(tcnsByMaxMode.GetList(baseTcnList));
+            var MinTcnList = new List<TcnDto>(tcnsByMinMode.GetList(baseTcnList));
+
+            Console.WriteLine("\n***List of tcn with max value****\n");
+            foreach (var t in MaxTcnList)
+                Console.WriteLine($"{t.Id} {t.Tcn}");
+
+            Console.WriteLine("\n***List of tcn with min value****\n");
+            foreach (var t in MinTcnList)
+                Console.WriteLine($"{t.Id} {t.Tcn}");
+
             Console.ReadKey();
         }
     }
